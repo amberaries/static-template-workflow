64dc07437c0304b55b1ce994f28b85a79ee6a157
Andrew N.Romanov <andrew.n.romanov@gmail.com>
2018-05-07 02:50:15 +0300

    Initial setup. Babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 "use strict";
 
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
+const gulp = require('gulp');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const yargs = require('yargs');
+
+const $ = require('gulp-load-plugins')();
+const PRODUCTION = !!(yargs.argv.production);
+
+const { COMPATIBILITY, PORT, PATHS } = loadConfig();
+function loadConfig() {
+  let ymlFile = fs.readFileSync('config.yml', 'utf8');
+  return yaml.load(ymlFile);
+}
 
 gulp.task('default', []);
