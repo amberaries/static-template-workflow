120b92b99f29d5fca0c58045e2e843c7e1516539
Andrew N.Romanov <andrew.n.romanov@gmail.com>
2018-05-07 03:52:57 +0300

    Generate page via panini

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,21 +1,39 @@
 "use strict";
 
-
-import gulp     from 'gulp';
 import plugins  from 'gulp-load-plugins';
+import yargs    from 'yargs';
 import browser  from 'browser-sync';
+import gulp     from 'gulp';
+import panini   from 'panini';
+import rimraf   from 'rimraf';
+import sherpa   from 'style-sherpa';
 import yaml     from 'js-yaml';
 import fs       from 'fs';
-import yargs    from 'yargs';
 
 const $ = plugins();
-
 const PRODUCTION = !!(yargs.argv.production);
-
 const { COMPATIBILITY, PORT, PATHS } = loadConfig();
+
 function loadConfig() {
   let ymlFile = fs.readFileSync('config.yml', 'utf8');
   return yaml.load(ymlFile);
 }
 
-gulp.task('default', []);
+function clean(done) {
+  rimraf(PATHS.dist, done);
+}
+
+function pages() {
+  return gulp.src('src/pages/**/*.{html,hbs,handlebars}')
+    .pipe(panini({
+      root: 'src/pages/',
+      layouts: 'src/layouts/',
+      partials: 'src/partials/',
+      data: 'src/data/',
+      helpers: 'src/helpers/'
+    }))
+    .pipe(gulp.dest(PATHS.dist));
+}
+
+gulp.task('build', gulp.series(clean, gulp.parallel(pages)));
+gulp.task('default', gulp.series('build'));
